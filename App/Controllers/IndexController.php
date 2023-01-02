1e87576311256c63612a8e36bed3c4f15fde30b7
Pedro_paulino <pedropaulinoeng@hotmail.com>
2023-01-02 11:22:46 -0300

    A519 - Add Registro usuarios

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -16,7 +16,57 @@ class IndexController extends Action{
 
     public function inscreverse(){
 
+        $this->view->erroCadastroEmail = false;
+        $this->view->erroCadastroPreenchimento = false;
+
         $this->render('inscreverse','layout');
+
+    }
+
+    public function registrar(){
+
+        $this->view->erroCadastroEmail = false;
+        $this->view->erroCadastroPreenchimento = false;
+        
+        $usuario = Container::getModel('usuario');
+
+        $usuario->__set("nome", $_POST['nome']);
+        $usuario->__set("email", $_POST['email']);
+        $usuario->__set("senha", $_POST['senha']);
+
+        if($usuario->validarCadastro()){
+
+       
+            if(count($usuario->getUsuarioPorEmail()) < 1){
+
+                $usuario->salvar();
+                $this->render('cadastro','layout');
+                
+            }else{
+
+               $this->view->erroCadastroEmail = true;
+                
+                $this->render('inscreverse','layout');
+            }
+
+        }else{
+
+            $this->view->erroCadastroPreenchimento = true;
+
+            $this->render('inscreverse','layout');
+
+        }
+
+    
+    
+
+
+               
+
+
+
+        
+
         
     }
 
--- App/Route.php
+++ App/Route.php
@@ -21,6 +21,12 @@ class Route extends Bootstrap{
             'action' => 'inscreverse'
         );
 
+        $routes['registrar'] = array(
+            'route' => '/registrar',
+            'controller' => 'indexController',
+            'action' => 'registrar'
+        );
+
         return $this->setRoutes($routes);
 
     }
